99bb62e50ddd3c83b05aa1d4d3d442669b9892ad
Jared Meyering <jared.meyering@gmail.com>
2017-05-26 10:51:15 -0500

    add sectioning to styleguide

--- styleguide.config.js
+++ styleguide.config.js
@@ -4,6 +4,24 @@ const webpackConfig = require('./webpack.config.js');
 module.exports = {
   components: 'src/components/**/*.jsx',
   styleguideDir: './public/styleguide',
+  sections: [
+    {
+      name: 'Atoms',
+      components: 'src/components/atoms/**/*.jsx',
+    },
+    {
+      name: 'Molecules',
+      components: 'src/components/molecules/**/*.jsx',
+    },
+    {
+      name: 'Organisms',
+      components: 'src/components/organisms/**/*.jsx',
+    },
+    {
+      name: 'Layout',
+      components: 'src/components/layout/**/*.jsx',
+    },
+  ],
   webpackConfig: Object.assign({}, webpackConfig, {
     resolve: {
       alias: {
