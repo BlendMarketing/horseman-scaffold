43995f195c227611de14fe13510868d0907a7f9a
Jared Meyering <jared.meyering@gmail.com>
2017-06-06 12:14:14 -0500

    add eslint test file

---
+++ eslint-test.js
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom";
+import React, {Component} from 'react';
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
+
+ReactDOM.render( <MyComponent />
+  , document.getElementById('root'));
