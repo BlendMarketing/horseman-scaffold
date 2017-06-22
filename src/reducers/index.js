82d4bf4fd5e29538bd3b95a05914645dd4a0388b
Jared Meyering <jared.meyering@gmail.com>
2017-06-22 16:52:19 -0500

    tweaks

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import { horsemanReducer } from 'horseman-core';
 
 export default combineReducers({
   router: routerReducer,
+  horsemanResources: horsemanReducer,
   // Add custom reducers here
 });
