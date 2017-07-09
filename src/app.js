738adc15c8b7bb03479e7fd184185a53f2124f43
Jared Meyering <jared.meyering@gmail.com>
2017-07-09 05:36:51 +0000

    tweaks to the scaffold

---
+++ src/app.js
@@ -0,0 +1,34 @@
+/**
+ * REACT COMPONENTS
+ */
+import React from 'react';
+import { AppContainer } from 'react-hot-loader';
+import { Provider } from 'react-redux';
+import createHistory from 'history/createBrowserHistory';
+import { ThemeProvider } from 'styled-components';
+
+import theme from './themes/main';
+import Router from './router';
+import configureStore from './store/configureStore';
+
+/**
+ * Establish your application's initial state here.
+ */
+const initialState = {
+};
+
+const history = createHistory();
+
+const store = configureStore(initialState, history);
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <AppContainer>
+      <Provider store={store}>
+        <Router history={history} store={store} />
+      </Provider>
+    </AppContainer>
+  </ThemeProvider>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,47 +3,22 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import { Provider } from 'react-redux';
-import createHistory from 'history/createBrowserHistory';
-import { ThemeProvider } from 'styled-components';
-
-import theme from './themes/main';
-import Router from './router';
-import configureStore from './store/configureStore';
-
-/**
- * Establish your application's initial state here.
- */
-const initialState = {
-};
-
-const history = createHistory();
-
-const store = configureStore(initialState, history);
-
-const App = () => (
-  <ThemeProvider theme={theme}>
-    <AppContainer>
-      <Provider store={store}>
-        <Router history={history} store={store} />
-      </Provider>
-    </AppContainer>
-  </ThemeProvider>
-);
+import App from './app';
 
 ReactDOM.render(
-  App(),
+  <App />,
   document.getElementById('root'),
 );
 
 
 // Hot Module Replacement API
 if (module.hot) {
-  module.hot.accept('./router', () => {
+  module.hot.accept('./app', () => {
     ReactDOM.render(
-      App(),
+      <App />,
       document.getElementById('root'),
     );
   });
 }
+
+export default App;
